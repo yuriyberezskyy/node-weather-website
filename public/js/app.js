ec05906f2514da9168dfa6470d8d7227feb26317
yuriyberezskyy <bereza95@gmail.com>
2021-03-16 20:53:51 -0400

    set up app to heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,22 +11,21 @@ weatherForm.addEventListener("submit", (e) => {
 
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-        } else {
-          const tempAndFeels =
-            "It is " +
-            data.temperature +
-            " Celsium degrees and feels like " +
-            data.feelslike +
-            " Celsium degrees";
-          messageOne.textContent = data.address;
-          messageTwo.textContent = tempAndFeels;
-        }
-      });
-    }
-  );
+
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        const tempAndFeels =
+          "It is " +
+          data.temperature +
+          " Celsium degrees and feels like " +
+          data.feelslike +
+          " Celsium degrees";
+        messageOne.textContent = data.address;
+        messageTwo.textContent = tempAndFeels;
+      }
+    });
+  });
 });
